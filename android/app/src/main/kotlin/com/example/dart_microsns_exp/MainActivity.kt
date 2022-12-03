441333f45ad9471f75a3355d3a42aef2ece480de
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-12-03 11:20:10 +0900

    Initialize

---
+++ android/app/src/main/kotlin/com/example/dart_microsns_exp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_microsns_exp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sql.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+var user = "f5fed5cc-1fdc-40bf-a258-0b933459e637";
+
+class NoteViewModel {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE note(
+      id INTEGER PRIMARY KEY,
+      pubid TEXT,
+      time TEXT,
+      subject TEXT,
+      flag INTEGER,
+      user TEXT,
+      description TEXT)
+    """);
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'note.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  static Future<int> createItem(String? subject, String? description) async {
+    final db = await NoteViewModel.db();
+    // UUID Key Generator
+    final pubid = uuid.v4();
+    final flag = 0;
+    final now = DateTime.now();
+    final data = {
+      'pubid': pubid,
+      'time': now,
+      'subject': subject,
+      'description': description,
+      'flag': flag,
+      'user': user
+    };
+    final id = await db.insert('items', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+  static Future<List<Map<String, dynamic>>> getNotes() async {
+    final db = await NoteViewModel.db();
+    return db.query('items', orderBy: "id");
+  }
+
+  static Future<List<Map<String, dynamic>>> getItem(int id) async {
+    final db = await NoteViewModel.db();
+    return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  static Future<int> updateItem(
+      int id, String title, String? descrption) async {
+    final db = await NoteViewModel.db();
+
+    final data = {
+      'title': title,
+      'description': descrption,
+      'createdAt': DateTime.now().toString()
+    };
+
+    final result =
+        await db.update('items', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
+
+  static Future<void> deleteItem(int id) async {
+    final db = await NoteViewModel.db();
+    try {
+      await db.delete("items", where: "id = ?", whereArgs: [id]);
+    } catch (err) {
+      debugPrint("Something went wrong when deleting an item: $err");
+    }
+  }
+}
